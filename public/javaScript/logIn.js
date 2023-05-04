5c2fa6d52042853b4105d9aa4eb9c3d93f31c78a
joseManuel1234 <jmanuelrardilla@gmail.com>
2023-05-03 23:47:48 -0500

    zi

--- public/javaScript/logIn.js
+++ public/javaScript/logIn.js
@@ -1,21 +1,20 @@
 // Inicializa Google Identity Services al cargar la página.
 window.onload = function () {
-    google.accounts.id.initialize({
+  google.accounts.id.initialize({
       client_id: '683330713919-4uncacmbb91tdm0c142e2esscrsbhfb2.apps.googleusercontent.com',
       callback: handleCredentialResponse,
       auto_prompt: false,
-    });
-  };
-  
-  // Inicia el flujo de inicio de sesión de Google al hacer clic en tu icono.
-  $(document).ready(function () {
-    $('.fa-google').click(function () {
-      google.accounts.id.prompt();
-    });
   });
-  
-  // Maneja la respuesta del inicio de sesión de Google.
-  function handleCredentialResponse(response) {
-    console.log(`Token: ${response.credential}`);
-  }
-  
+};
+
+// Inicia el flujo de inicio de sesión de Google al hacer clic en tu icono.
+$(document).ready(function () {
+  $('.fa-google').click(function () {
+      google.accounts.id.prompt();
+  });
+});
+
+// Maneja la respuesta del inicio de sesión de Google.
+function handleCredentialResponse(response) {
+  console.log(`Token: ${response.credential}`);
+}
