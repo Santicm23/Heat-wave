76d009f27d96464e3324a149223d944b7977ead6
Santicm23 <santicastrom@outlook.com>
2023-05-04 11:05:11 -0500

    conectando front con back-end

--- public/javaScript/logIn.js
+++ public/javaScript/logIn.js
@@ -10,15 +10,14 @@ const inputPass = document.querySelector('#pass');
 
 // Inicializa Google Identity Services al cargar la página.
 window.onload = () => {
-  google.accounts.id.initialize({
-      client_id: '683330713919-4uncacmbb91tdm0c142e2esscrsbhfb2.apps.googleusercontent.com',
-      callback: handleCredentialResponse,
-      auto_prompt: false,
+    google.accounts.id.initialize({
+        client_id: '683330713919-4uncacmbb91tdm0c142e2esscrsbhfb2.apps.googleusercontent.com',
+        callback: handleCredentialResponse,
+        auto_prompt: false,
     });
   };
-
+  
 // Inicia el flujo de inicio de sesión de Google al hacer clic en tu icono.
-
 $('.fa-google').click(function () {
     google.accounts.id.prompt();
 });
