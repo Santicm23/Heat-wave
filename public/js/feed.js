0d035c71fc92f67c522137345fdd971a782ca55e
lau052004 <ovalle.laura@javeriana.edu.co>
2023-05-11 14:36:31 -0500

    acepto a santi

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,32 +1,3 @@
-
-const url = `http://${window.location.host}`;
-const userName = document.querySelector('#userName');
-
-const main = async() => {
-    const token = localStorage.getItem('token');
-    
-    const resp = await fetch(`${url}/auth`, {
-        headers: {
-            'x-token': token
-        }
-    });
-
-    console.log(await resp.json());
-    
-    // const { account, token: newToken } = await resp.json();
-    // localStorage.setItem('token', newToken);
-    
-    // userName.textContent = `@${account.username}`
-} 
-
-main();
-
-
-
-// fetch(`${url}/645ba7c00a6f567046ce6132`)
-//     .then((res) => res.blob())
-//     .then()
-
 const url = `http://${window.location.host}/songs/track`;
 
 fetch(`${url}/24`)
