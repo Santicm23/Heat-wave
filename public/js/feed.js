6f6f94bbf95004ad43927a6bdc77df9dc74bcf72
Santicm23 <santicastrom@outlook.com>
2023-05-11 17:54:43 -0500

    solicitud de ejemplo en el front end

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,8 +1,36 @@
 
+const url = `http://${window.location.host}`;
 
-const url = `http://${window.location.host}/songs/track`;
+const username = document.getElementById('username');
+const name = document.getElementById('name');
 
-fetch(`${url}/24`)
+let sesionToken = localStorage.getItem('token');
+
+console.log(sesionToken);
+
+fetch(`${url}/auth/`, {
+    headers: {
+        'x-token': sesionToken
+    }
+})
+.then(resp => {
+    if (resp.ok) {
+        return resp.json();
+    } else {
+        throw new Error('Error con el token');
+    }
+})
+.then(data => {
+    sesionToken = data.token;
+    username.textContent = `@${data.account.username}`;
+    name.textContent = data.account.name;
+})
+.catch(err => {
+    console.error(err);
+    window.location = 'index.html';
+});
+
+fetch(`${url}/songs/track/24`)
     .then(resp => {
         if (resp.ok) {
             return resp.blob();
