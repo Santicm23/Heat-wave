0365cf5d171db68d522e8ecb436f5e48a938248b
Santicm23 <santicastrom@outlook.com>
2023-05-18 10:04:38 -0500

    test iniciar sesión

--- tests/selenium.test.ts
+++ tests/selenium.test.ts
@@ -6,14 +6,30 @@ const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
+jest.setTimeout(10000);
+
 describe('Interactive tests with selenium', () => {
     
     test('Selenium should be able to login', async() => {
         await driver.get('http://localhost:8080');
 
-        await driver.findElement(webdriver.By.name('q')).sendKeys('webdriver\n');
+        const first_window = await driver.getCurrentUrl();
+
+        const name = await driver.findElement(webdriver.By.id('un-email'));
+        name.sendKeys('username');
+        const pass = await driver.findElement(webdriver.By.id('pass'));
+        pass.sendKeys('123456');
         
-        driver.quit();
+        const btnLogin = await driver.findElement(webdriver.By.id('btnLogin'));
+        await btnLogin.click();
+
+        await driver.sleep(1000);
+        
+        expect(await driver.getCurrentUrl()).not.toBe(first_window);
+
+        await driver.sleep(1000);
+        
+        await driver.quit();
     });
     
 });
